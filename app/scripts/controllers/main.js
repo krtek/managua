27e8fbce46331a0260821f72381e8bd2d284763e
Lukáš Marek <lukas.marek@fragaria.cz>
2015-10-21 10:13:48 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name managuaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the managuaApp
+ */
+angular.module('managuaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
